bfbd6ac75bd8adb618ab38e1dc3dd14e765b459e
SirAlexLopez <johan.lopez.ortiz@gmail.com>
2021-08-14 22:07:02 -0500

    First commit - version 1.0 crea y lee producto, falta borrar y actualizar

---
+++ src/main/java/com/ecommerce/product_ms/exceptions/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ecommerce.product_ms.exceptions;
+
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
