cacf216a1608695d194a6f096e4d93596de39445
SirAlexLopez <johan.lopez.ortiz@gmail.com>
2021-08-27 19:57:12 -0500

    product ms new orden true V1

--- src/main/java/com/ecommerce/product_ms/models/DetailOrder.java
+++ src/main/java/com/ecommerce/product_ms/models/DetailOrder.java
@@ -3,18 +3,28 @@ package com.ecommerce.product_ms.models;
 public class DetailOrder {
     private String idProduct;
     private Integer quantity;
+    private String name;
     private Long price;
     private Long subTotal;
 
-    public DetailOrder(String idProduct, Integer quantity, Long price, Long subTotal) {
+    public DetailOrder(String idProduct, String name, Integer quantity, Long price, Long subTotal) {
         this.idProduct = idProduct;
         this.quantity = quantity;
         this.price = price;
         this.subTotal = subTotal;
+        this.name= name;
     }
 
     public String getIdProduct() {
         return idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setIdProduct(String idProduct) {
